957d8b00a800ae28751268415f6e1b3ce4417667
Alex Zirbel <alex.zirbel@addepar.com>
2015-01-14 20:03:51 -0800

    Added bar example

---
+++ tests/dummy/app/controllers/bars.js
@@ -0,0 +1,38 @@
+/* Tell JSHint not to worry about _ not being defined */
+/* global _:false */
+
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  columns: Ember.computed(function() {
+    var colors, column1, columns;
+    colors = ['blue', 'teal', 'green', 'yellow', 'orange'];
+    column1 = Ember.Table.ColumnDefinition.create({
+      savedWidth: 50,
+      headerCellName: 'Name',
+      contentPath: 'key'
+    });
+    columns = colors.map(function(color, index) {
+      return Ember.Table.ColumnDefinition.create({
+        color: color,
+        headerCellName: 'Bar',
+        tableCellViewClass: 'bar-table-cell',
+        contentPath: "value" + (index + 1)
+      });
+    });
+    columns.unshift(column1);
+    return columns;
+  }),
+  content: Ember.computed(function() {
+    return _.range(100).map(function(index) {
+      return {
+        key: index,
+        value1: Math.random() * 80 + 10,
+        value2: Math.random() * 80 + 10,
+        value3: Math.random() * 80 + 10,
+        value4: Math.random() * 80 + 10,
+        value5: Math.random() * 80 + 10
+      };
+    });
+  })
+});
