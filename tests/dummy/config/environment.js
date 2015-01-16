424784fb96e845feda691e1f3fde6d1d6341cbb0
Alex Zirbel <alex.zirbel@addepar.com>
2015-01-16 15:05:41 -0800

    Use grunt-release-it to build gh-pages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -49,6 +49,11 @@ module.exports = function(environment) {
     }
   };
 
+  // Makes the app work when deployed to gh-pages
+  if (environment === 'production') {
+    ENV.baseURL = '/ember-table-addon/';
+  }
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
