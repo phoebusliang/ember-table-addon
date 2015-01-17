82820e3cb7edb4d52a8564eb609d74ae76bea91e
Alex Zirbel <alex.zirbel@addepar.com>
2015-01-16 18:27:10 -0800

    WIP text replace

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 /* global require, module */
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var fs = require('fs');
 
 var app = new EmberAddon({
   lessOptions: {
@@ -8,8 +9,20 @@ var app = new EmberAddon({
       'tests/dummy/app/styles/'
     ],
     outputFile: 'dummy.css'
+  },
+
+  // Replace patterns starting with @@
+  replace: {
+    files: [
+      '**/*'
+    ],
+    patterns: [{
+      match: /@@{(foo)}/,
+      replacement: 'Bam $1 Bam'
+    }]
   }
 });
 
 app.import(app.bowerDirectory + '/d3/d3.js');
+
 module.exports = app.toTree();
