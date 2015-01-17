4a0600aff04e0955dd99ed1457a209b3af74f7ef
Alex Zirbel <alex.zirbel@addepar.com>
2015-01-17 12:48:53 -0800

    What is going on

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,11 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 var fs = require('fs');
 
+// I know this is a hack, but it DOES work. I'll replace it later.
+function htmlEntities(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
 var app = new EmberAddon({
   lessOptions: {
     paths: [
@@ -23,10 +28,27 @@ var app = new EmberAddon({
     patterns: [{
       match: /@@{.*}/,
       replacement: function(matchedText) {
-        filename = matchedText.slice(3, -1);
-        fullFilename = './tests/dummy/app/' + filename;
-        fileContents = fs.readFileSync('./test.txt', 'utf8');
-        return fileContents.replace(/\n/g, '\\n');
+        // I'm going insane
+        var matchedLength = matchedText.length;
+        var cutLength = matchedLength - 4;
+
+        // If I do this, everything works fine
+        var filename = matchedText.substr(3, 21);
+
+        // If I do this, this function returns 21
+        // return cutLength;
+
+        // BUT If I do this, the console spits out major errors. It appears the
+        // string has overflowed.
+        var filename = matchedText.substr(3, cutLength);
+
+        var fullFilename = './tests/dummy/app/' + filename;
+        var fileContents = fs.readFileSync(fullFilename, 'utf8');
+
+        // Incidentally, this also works
+        //fileContents = fs.readFileSync('./tests/dummy/app/controllers/simple.js', 'utf8');
+
+        return htmlEntities(fileContents).replace(/\n/g, '\\n');
       }
     }]
   }
