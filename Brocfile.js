f0a0acc46c7ab12800b44c25941a071696792b79
Alex Zirbel <alex.zirbel@addepar.com>
2015-01-19 16:44:25 -0800

    File replace done, thanks Erik

--- Brocfile.js
+++ Brocfile.js
@@ -3,11 +3,6 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 var fs = require('fs');
 
-// I know this is a hack, but it DOES work. I'll replace it later.
-function htmlEntities(str) {
-  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
 var app = new EmberAddon({
   lessOptions: {
     paths: [
@@ -28,21 +23,10 @@ var app = new EmberAddon({
     patterns: [{
       match: /@@{[^}]*}/,
       replacement: function(matchedText) {
-        // I'm going insane
-        var matchedLength = matchedText.length;
-        var cutLength = matchedLength - 4;
-
-        // BUT If I do this, the console spits out major errors. It appears the
-        // string has overflowed.
-        var filename = matchedText.substr(3, cutLength);
-
-        var fullFilename = './tests/dummy/app/' + filename;
-        var fileContents = fs.readFileSync(fullFilename, 'utf8');
-
-        // Incidentally, this also works
-        //fileContents = fs.readFileSync('./tests/dummy/app/controllers/simple.js', 'utf8');
-
-        return htmlEntities(fileContents).replace(/\n/g, '\\n');
+        filename = matchedText.slice(3, -1);
+        fullFilename = './tests/dummy/app/' + filename;
+        fileContents = fs.readFileSync(fullFilename, 'utf8');
+        return fileContents.replace(/\n/g, '\\n');
       }
     }]
   }
