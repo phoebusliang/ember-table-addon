8c85a2b2adb2c39b34b83d6d0e023f83222ccc56
Alex Zirbel <alex.zirbel@addepar.com>
2015-03-16 14:00:33 -0700

    Convert addon/controllers folder

--- addon/controllers/row.js
+++ addon/controllers/row.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
-// TODO(azirbel): Should be a controller
-export default Ember.Table.Row;
+export default Ember.ObjectProxy.extend({
+  content: null,
+
+  isShowing: true,
+  isHovered: false,
+
+  isSelected: Ember.computed(function(key, val) {
+    if (arguments.length > 1) {
+      this.get('parentController').setSelected(this, val);
+    }
+    return this.get('parentController').isSelected(this);
+  }).property('parentController.selection.[]')
+});
