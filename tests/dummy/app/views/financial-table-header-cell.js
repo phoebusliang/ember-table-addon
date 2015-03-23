2d6289cad7d231474e1a1f0af8f851d290e976df
Alex Zirbel <alex.zirbel@addepar.com>
2015-03-23 14:48:39 -0700

    Fix header cells

--- tests/dummy/app/views/financial-table-header-cell.js
+++ tests/dummy/app/views/financial-table-header-cell.js
@@ -1,5 +1,5 @@
-import TableCell from '../views/table-cell';
+import HeaderCell from '../views/header-cell';
 
-export default TableCell.extend({
+export default HeaderCell.extend({
   templateName: 'financial-table/financial-table-header-cell'
 });
