06296e083225aab969d009cb0bf312872a18e504
Alex Zirbel <alex.zirbel@addepar.com>
2015-03-24 12:58:36 -0700

    Use more general bower syntax to import antiscroll

--- blueprints/ember-table/index.js
+++ blueprints/ember-table/index.js
@@ -6,7 +6,7 @@ module.exports = {
     // FIXME(azirbel): Do we need to install lodash too?
     return this.addBowerPackagesToProject([
       {
-        'name': 'git@github.com:azirbel/antiscroll.git#90391fb371c7be769bc32e7287c5271981428356'
+        'name': 'git://github.com/azirbel/antiscroll.git#90391fb371c7be769bc32e7287c5271981428356'
       },
       {
         'name': 'jquery-mousewheel',
