92ab71a99aef52391e2d2adb289858bf07d72a5e
Alex Zirbel <alex.zirbel@addepar.com>
2015-04-09 14:07:14 -0700

    WIP again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
 
     app.import(app.bowerDirectory + '/antiscroll/antiscroll.js');
     app.import(app.bowerDirectory + '/antiscroll/antiscroll.css');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/jquery-ui.custom.js');
+    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.js');
     app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
   },
 
